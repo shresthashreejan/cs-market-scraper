7b067ba379acfbe57935c4d92fa39f7cd2475171
shreejan shrestha <shr.shreejan@gmail.com>
2024-08-17 20:23:33 +0545

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from playwright.sync_api import sync_playwright
+
+def main():
+    with sync_playwright() as p:
+        try:
+            browser = p.chromium.launch(headless=False, slow_mo=100)
+            page = browser.new_page()
+            page.goto('https://skinflow.gg')
+            page.get_by_role("navigation").get_by_role("link", name="Buy").click()
+            page.screenshot(path='debug.png')
+            browser.close()
+        except Exception as e:
+            print(e)
+
+if __name__ == "__main__":
+    main()
